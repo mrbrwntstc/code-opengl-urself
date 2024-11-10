0178f2958d0b3ffddcbceb1e94d232f3d26a11fd
Gaurav Bhaskar <geekman95@gmail.com>
2024-11-10 10:00:31 -0600

    window abstraction

--- tetris/main.cpp
+++ tetris/main.cpp
@@ -1,57 +1,15 @@
-#define GLAD_GL_IMPLEMENTATION
-#include <glad/glad.h>
-#define GLFW_INCLUDE_NONE
-#include <GLFW/glfw3.h>
-
-#include <stdlib.h>
-#include <stddef.h>
-#include <stdio.h>
-
-static void error_callback(int error, const char* description)
-{
-  fprintf(stderr, "Error: %s\n", description);
-}
-
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-      glfwSetWindowShouldClose(window, GLFW_TRUE);
-}
+#include "window.h"
 
 int main(void)
 {
-  glfwSetErrorCallback(error_callback);
+  window::init();
 
-  if (!glfwInit())
-      exit(EXIT_FAILURE);
-
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-#ifdef __APPLE__
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
-#endif // __APPLE__
-
-  GLFWwindow* window = glfwCreateWindow(640, 480, "Tetris - OpenGL", NULL, NULL);
-  if (!window)
+  while (!window::should_close())
   {
-      glfwTerminate();
-      exit(EXIT_FAILURE);
-  }
-  glfwSetKeyCallback(window, key_callback);
-
-  glfwMakeContextCurrent(window);
-  gladLoadGL();
-  glfwSwapInterval(1);
-
-  while (!glfwWindowShouldClose(window))
-  {
-    glfwSwapBuffers(window);
-    glfwPollEvents();
+    window::flush();
   }
 
-  glfwDestroyWindow(window);
+  window::cleanup();
 
-  glfwTerminate();
-  exit(EXIT_SUCCESS);
+  return 0;
 }
